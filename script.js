27297e0d652010dd84a5f87647f9951bce5bedba
Marcus <MarcusIsCode@users.noreply.github.com>
2019-11-26 21:48:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 const container = document.querySelector('.container')
 let cardsArray =[];
 let compareArray = [];// för att gemföra 2 värden
-let count = 0;
+let wrongCardArray =[];
+let count = 1;
 let countMoves = 0;
 for (let i = 0; i < 16; i++) {
     const card = document.createElement("div");
     
     card.classList.add("level1");
+    card.style.color ="tan"
     card.textContent = (i+i)%16;
     cardsArray[i] = card;
+    
     
 }
 
@@ -18,22 +21,39 @@ for (let i = 0; i < 16; i++) {
     for (let index = 0; index < cardsArray.length; index++) {
          cardsArray[index].addEventListener('click', (event) => {
            countMoves++;
-           console.log(countMoves);
+          // console.log(countMoves);
             const x = event.target.innerHTML;
-            const y = event.target.innerHTML;
-            console.log(x);
-            if (count ==2){
-                console.log(y)
-            }
-            if(count ===2){
-                count = 1;
-            }else{
-                count++;
-            } 
+            compareArray.push(x);
             
-            if(y === x){
-                console.log('hello');
-            }
+            const y = event.target;
+            y.style.color ="black";
+            wrongCardArray.push(y);
+          
+             let card1 =compareArray[0];
+             let card2 = compareArray[1];
+            
+          
+            
+                if (card1 === card2) {
+                    compareArray = [];
+                    wrongCardArray = [];
+                    count = 1;
+                } else if(card1 !== card2 && count ===2)  {
+                    wrongCardArray.forEach(wrong => {
+                        wrong.style.color='tan';//sätta någon delay
+                    });
+                    compareArray = [];
+                    count =1;
+                  
+                    
+                }else{
+                    count++;
+                    console.log(count)
+                }
+
+            
+           
+           
         
         }) 
     
